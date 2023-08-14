a134655c50a55f4c893d18b1326c067bbd1ba82c
Remco Hoff <remco@code010.nl>
2023-08-14 15:49:24 +0200

    Added formvalidation to afhandelform

--- app/frontend/assets/controllers/meldingbehandelformulier_controller.js
+++ app/frontend/assets/controllers/meldingbehandelformulier_controller.js
@@ -1,5 +1,10 @@
 import { Controller } from '@hotwired/stimulus';
 
+let form = null
+let inputList = null
+let formData = null
+const defaultErrorMessage = "Vul a.u.b. dit veld in."
+
 export default class extends Controller {
     static values = {
         formIsSubmitted: Boolean,
@@ -9,18 +14,63 @@ export default class extends Controller {
     static targets = ["externalText", "internalText"]
 
     connect() {
-        console.log("melding behandel formulier connected ", this.standaardafhandeltekstenValue)
         if(this.hasExternalTextTarget) {
-            console.log("bestaat")
             if(this.externalTextTarget.textContent.length > 0) {
                 this.externalMessage = this.externalTextTarget.textContent
             }
         }
 
-        // this.element.dispatchEvent(new CustomEvent("formHandleIsConnected", {
-        //     detail: JSON.parse(this.parentContextValue),
-        //     bubbles: true
-        // }));
+        form = document.querySelector("#afhandelForm");
+        inputList = document.querySelectorAll('.js-validation textarea')
+        formData = new FormData(form)
+
+        for (let i=0; i<inputList.length; i++){
+            const input = inputList[i]
+            const error = input.closest('.form-row').getElementsByClassName('invalid-text')[0]
+            input.addEventListener("input", (event) => {
+                if (input.validity.valid) {
+                    input.closest('.form-row').classList.remove('is-invalid')
+                    error.textContent = "";
+                } else {
+                    error.textContent = defaultErrorMessage;
+                    input.closest('.form-row').classList.add('is-invalid')
+                }
+            })
+        };
+
+        form.addEventListener("submit", (event) => {
+            const allFieldsValid = this.checkValids()
+
+            if(!(allFieldsValid)){
+                const errorList = document.querySelectorAll('div.is-invalid')
+                errorList[0].scrollIntoView({ behavior: "smooth"})
+                event.preventDefault();
+            }
+        });
+    }
+
+    checkValids() {
+        //check all inputfields (except checkboxes) for validity
+        // if 1 or more fields is invalid, don't send the form (return false)
+        inputList = document.querySelectorAll('textarea')
+        let count = 0
+        for (let i=0; i<inputList.length; i++){
+            const input = inputList[i]
+            const error = input.closest('.form-row').getElementsByClassName('invalid-text')[0]
+            if (input.validity.valid) {
+                error.textContent = "";
+                input.closest('.form-row').classList.remove('is-invalid')
+            } else {
+                error.textContent = defaultErrorMessage;
+                input.closest('.form-row').classList.add('is-invalid')
+                count++
+            }
+        }
+        if (count > 0) {
+            return false
+        }else {
+            return true
+        }
     }
 
     cancelHandle() {
