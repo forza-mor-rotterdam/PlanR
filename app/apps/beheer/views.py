e227e9b85c6b22e1200ce32d5decd8aa69eac6bb
Maurice Guikema <mguikema@gmail.com>
2023-09-25 15:22:19 +0200

    beheer permissions

--- app/apps/beheer/views.py
+++ app/apps/beheer/views.py
@@ -1,7 +1,9 @@
 from apps.authenticatie.forms import GebruikerAanmakenForm, GebruikerAanpassenForm
 from django.contrib.auth import get_user_model
+from django.contrib.auth.decorators import login_required, permission_required
 from django.shortcuts import render
 from django.urls import reverse_lazy
+from django.utils.decorators import method_decorator
 from django.views import View
 from django.views.generic.edit import CreateView, UpdateView
 from django.views.generic.list import ListView
@@ -9,6 +11,7 @@ from django.views.generic.list import ListView
 Gebruiker = get_user_model()
 
 
+@permission_required("authorisatie.beheer_bekijken")
 def beheer(request):
     return render(
         request,
@@ -22,6 +25,7 @@ class GebruikerView(View):
     success_url = reverse_lazy("gebruiker_lijst")
 
 
+@method_decorator(login_required, name="dispatch")
 class GebruikerLijstView(GebruikerView, ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -33,6 +37,7 @@ class GebruikerLijstView(GebruikerView, ListView):
         return context
 
 
+@method_decorator(login_required, name="dispatch")
 class GebruikerAanmakenAanpassenView(GebruikerView):
     def form_valid(self, form):
         if not hasattr(form.instance, "profiel"):
@@ -49,6 +54,7 @@ class GebruikerAanmakenAanpassenView(GebruikerView):
         return super().form_valid(form)
 
 
+@method_decorator(login_required, name="dispatch")
 class GebruikerAanpassenView(GebruikerAanmakenAanpassenView, UpdateView):
     form_class = GebruikerAanpassenForm
     template_name = "authenticatie/gebruiker_aanpassen.html"
@@ -62,15 +68,7 @@ class GebruikerAanpassenView(GebruikerAanmakenAanpassenView, UpdateView):
         return initial
 
 
+@method_decorator(login_required, name="dispatch")
 class GebruikerAanmakenView(GebruikerAanmakenAanpassenView, CreateView):
     template_name = "authenticatie/gebruiker_aanmaken.html"
     form_class = GebruikerAanmakenForm
-
-    def form_invalid(self, form):
-        context = self.get_context_data(form=form)
-        print("error")
-        print(form.errors)
-        context["errors"] = form.errors
-        print(context)
-        # context.update({"my_message": "Soemthign went wrong"})
-        return self.render_to_response(context)
