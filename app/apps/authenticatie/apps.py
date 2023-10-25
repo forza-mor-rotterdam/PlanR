2e6576a0f5fb70e437e2fefb561af7ecb4a212e3
Maurice Guikema <maurice@mgui.nl>
2023-10-25 15:27:23 +0200

    disable creation of context for new prodiel

--- app/apps/authenticatie/apps.py
+++ app/apps/authenticatie/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class AuthenticatieConfig(AppConfig):
     name = "apps.authenticatie"
     verbose_name = "Authenticatie"
+
+    def ready(self):
+        import apps.authenticatie.signal_receivers  # noqa
