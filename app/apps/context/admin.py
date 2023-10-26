cbc927bdb6dc9d4c01df844e3e006b239df8cc74
Maurice Guikema <maurice@mgui.nl>
2023-10-26 13:42:23 +0200

    admin title

--- app/apps/context/admin.py
+++ app/apps/context/admin.py
@@ -1,5 +1,10 @@
 from apps.context.models import Context
 from django.contrib import admin
+from django.contrib.admin import AdminSite
+
+AdminSite.site_title = "PlanR Admin"
+AdminSite.site_header = "PlanR Admin"
+AdminSite.index_title = "PlanR Admin"
 
 
 class ContextAdmin(admin.ModelAdmin):
