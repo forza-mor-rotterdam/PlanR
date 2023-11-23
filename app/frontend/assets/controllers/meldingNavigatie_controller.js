dc33b0f0b96fba9f12e02cbb78701ce799be1adf
Remco Hoff <remco@code010.nl>
2023-11-23 10:40:52 +0100

    Hide navigation when there is only 1 item

--- app/frontend/assets/controllers/meldingNavigatie_controller.js
+++ app/frontend/assets/controllers/meldingNavigatie_controller.js
@@ -1,5 +1,8 @@
 import { Controller } from '@hotwired/stimulus'
 
+// let meldingIdList = []
+// let previousId = 0
+// let nextId = 0
 export default class extends Controller {
   static values = {
     meldingUuid: String,
@@ -9,33 +12,33 @@ export default class extends Controller {
   static targets = ['navigatie', 'teller', 'vorige', 'volgende']
 
   initialize() {
-    const meldingIdList = sessionStorage.getItem('meldingIdList').split(',')
-    const meldingId = this.meldingUuidValue
-    const isCurrentTask = (id) => id === meldingId
-    const index = meldingIdList.findIndex(isCurrentTask)
-    const url = this.urlValue
-    let previousId = 0
-    let nextId = 0
+    const meldingIdString = sessionStorage.getItem('meldingIdList') || ''
+    const meldingIdList = meldingIdString.split(',')
+    if (meldingIdList.length > 1) {
+      const meldingId = this.meldingUuidValue
+      const isCurrentTask = (id) => id === meldingId
+      const index = meldingIdList.findIndex(isCurrentTask)
+      const url = this.urlValue
 
-    this.tellerTarget.innerHTML = `${index + 1} van ${meldingIdList.length}`
+      this.tellerTarget.innerHTML = `${index + 1} van ${meldingIdList.length}`
 
-    previousId = meldingIdList[index - 1]
-    nextId = meldingIdList[index + 1]
+      const previousId = meldingIdList[index - 1]
+      const nextId = meldingIdList[index + 1]
 
-    if (meldingIdList.length < 2) {
+      if (previousId) {
+        this.vorigeTarget.setAttribute('href', `${url}${previousId}`)
+        this.vorigeTarget.classList.remove('disabled')
+      } else {
+        this.vorigeTarget.classList.add('disabled')
+      }
+      if (nextId) {
+        this.volgendeTarget.setAttribute('href', `${url}${nextId}`)
+        this.volgendeTarget.classList.remove('disabled')
+      } else {
+        this.volgendeTarget.classList.add('disabled')
+      }
+    } else {
       this.navigatieTarget.classList.add('hidden')
-    }
-    if (previousId) {
-      this.vorigeTarget.setAttribute('href', `${url}${previousId}`)
-      this.vorigeTarget.classList.remove('disabled')
-    } else {
-      this.vorigeTarget.classList.add('disabled')
-    }
-    if (nextId) {
-      this.volgendeTarget.setAttribute('href', `${url}${nextId}`)
-      this.volgendeTarget.classList.remove('disabled')
-    } else {
-      this.volgendeTarget.classList.add('disabled')
     }
   }
 }
