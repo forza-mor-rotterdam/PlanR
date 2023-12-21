d9e7a7f807d1c147dc823ef72ec9e17be9b715c4
Jorrit Strikwerda <jippiejorrit@gmail.com>
2023-12-21 12:01:53 +0100

    Fix tests.

--- app/apps/main/tests/tests_forms.py
+++ app/apps/main/tests/tests_forms.py
@@ -1,11 +1,22 @@
 from apps.main.forms import MeldingAfhandelenForm
+from apps.main.models import StandaardExterneOmschrijving
 from django.test import TestCase
 
 
 class MeldingAfhandelenFormTest(TestCase):
+    def setUp(self):
+        (
+            self.standaard_omschrijving,
+            _created,
+        ) = StandaardExterneOmschrijving.objects.get_or_create(
+            titel="Standaard afhandelreden",
+            tekst="Deze melding is behandeld. Bedankt voor uw inzet om Rotterdam schoon, heel en veilig te houden.",
+        )
+
     def test_omschrijving_extern_te_lang(self):
         form_data = {
             "omschrijving_extern": "A" * 1001,
+            "standaard_omschrijvingen": self.standaard_omschrijving,
         }
         form = MeldingAfhandelenForm(data=form_data)
         self.assertFalse(form.is_valid())
@@ -13,6 +24,8 @@ class MeldingAfhandelenFormTest(TestCase):
     def test_omschrijving_extern_valide(self):
         form_data = {
             "omschrijving_extern": "A" * 500,
+            "standaard_omschrijvingen": self.standaard_omschrijving,
         }
         form = MeldingAfhandelenForm(data=form_data)
+        print(form.errors)
         self.assertTrue(form.is_valid())
