e0f9b73c0e6f99b7459d90a9941cd3600aa57942
Jorrit Strikwerda <jippiejorrit@gmail.com>
2023-12-21 11:51:46 +0100

    Fix test

--- app/apps/main/tests/tests_forms.py
+++ app/apps/main/tests/tests_forms.py
@@ -5,14 +5,14 @@ from django.test import TestCase
 class MeldingAfhandelenFormTest(TestCase):
     def test_omschrijving_extern_te_lang(self):
         form_data = {
-            "omschrijving_extern": "A" * 2001,
+            "omschrijving_extern": "A" * 1001,
         }
         form = MeldingAfhandelenForm(data=form_data)
         self.assertFalse(form.is_valid())
 
     def test_omschrijving_extern_valide(self):
         form_data = {
-            "omschrijving_extern": "A" * 1000,
+            "omschrijving_extern": "A" * 500,
         }
         form = MeldingAfhandelenForm(data=form_data)
         self.assertTrue(form.is_valid())
