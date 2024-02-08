62f1e2165394e0c230cdb599aaab82ea872a361c
Remco Hoff <remco@code010.nl>
2024-02-08 13:09:24 +0100

    B&C fixes

--- app/apps/context/templatetags/main_tags.py
+++ app/apps/context/templatetags/main_tags.py
@@ -4,12 +4,19 @@ register = template.Library()
 
 
 @register.simple_tag
-def slice_iterable(input, start_int, end_int):
+def slice_iterable(input, start_int=None, end_int=None):
     try:
         iter(input)
     except TypeError:
         return input
-    if not isinstance(start_int, int) or not isinstance(end_int, int):
+    if start_int is not None and not isinstance(start_int, int):
         return input
-
+    if end_int is not None and not isinstance(end_int, int):
+        return input
+    if not start_int and not end_int:
+        return input
+    if start_int is None:
+        return input[:end_int]
+    if end_int is None:
+        return input[start_int:]
     return input[start_int:end_int]
