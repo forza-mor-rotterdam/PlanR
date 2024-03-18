1a6b39a765ea557f6447f27880cd8445c5be5748
Maurice Guikema <maurice@mgui.nl>
2024-03-18 11:15:34 +0100

    send user data to mor-core

--- app/apps/authenticatie/signal_receivers.py
+++ app/apps/authenticatie/signal_receivers.py
@@ -1,4 +1,5 @@
 from apps.authenticatie.models import Profiel
+from apps.services.meldingen import MeldingenService
 from django.contrib.auth import get_user_model
 from django.db.models.signals import post_save
 from django.dispatch import receiver
@@ -8,5 +9,8 @@ User = get_user_model()
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
+    MeldingenService().set_gebruiker(
+        gebruiker=instance.serialized_instance(),
+    )
     if not hasattr(instance, "profiel"):
         Profiel.objects.create(gebruiker=instance)
