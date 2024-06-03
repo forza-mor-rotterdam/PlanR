7a5ae777ee5c9aa5211bf9a8f89a03289910a98f
Maurice Guikema <maurice@mgui.nl>
2024-06-03 19:44:36 +0200

    beheer env urls

--- app/apps/beheer/views.py
+++ app/apps/beheer/views.py
@@ -1,11 +1,38 @@
+from django.conf import settings
 from django.contrib.auth.decorators import permission_required
 from django.shortcuts import render
 
 
 @permission_required("authorisatie.beheer_bekijken", raise_exception=True)
 def beheer(request):
+    env_suffixs = {
+        settings.PRODUCTIE: "",
+        settings.ACCEPTATIE: "-acc",
+        settings.TEST: "-test",
+    }
+    beheer_url = "/beheer/"
+    domain = "forzamor.nl"
+
+    env_urls = [
+        # (f"https://planr{env_suffixs.get(settings.APP_ENV, env_suffixs.get(settings.ACCEPTATIE))}.{domain}{beheer_url}", "PlanR"),
+        (
+            f"https://fixer{env_suffixs.get(settings.APP_ENV, env_suffixs.get(settings.ACCEPTATIE))}.{domain}{beheer_url}",
+            "FixeR",
+        ),
+        (
+            f"https://onderwerpen{env_suffixs.get(settings.APP_ENV, env_suffixs.get(settings.ACCEPTATIE))}.{domain}{beheer_url}",
+            "Onderwerpen",
+        ),
+        (
+            f"https://ontdbblr{env_suffixs.get(settings.APP_ENV, env_suffixs.get(settings.ACCEPTATIE))}.{domain}{beheer_url}",
+            "OntdbblR",
+        ),
+    ]
+
     return render(
         request,
-        "beheer.html",
-        {},
+        "beheer/beheer.html",
+        {
+            "env_urls": env_urls,
+        },
     )
