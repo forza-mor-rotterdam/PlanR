eaef54bb1734e17a06f914a4a06316bd029a9445
Remco Hoff <remco@code010.nl>
2024-10-01 14:28:37 +0200

    Styling, save fold in/out

--- app/frontend/assets/controllers/navigation_controller.js
+++ app/frontend/assets/controllers/navigation_controller.js
@@ -5,11 +5,18 @@ export default class extends Controller {
 
   initialize() {}
 
-  connect() {}
+  connect() {
+    console.log(sessionStorage.getItem('navSmall'))
+    this.element.classList.add(sessionStorage.getItem('navSmall'))
+  }
 
   toggleMenu(e) {
-    console.log(e)
     this.element.classList.toggle('nav--small')
+    console.log(sessionStorage.getItem('navSmall') === 'true')
+    let navSize = this.element.classList.contains('nav--small')
+      ? 'nav--small'
+      : 'sessionStorage.setItem'
+    sessionStorage.setItem('navSmall', navSize)
     e.target.closest('button').blur()
   }
 }
