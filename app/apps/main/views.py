74389743ebcfab608815936991190fdd87358aa0
Remco Hoff <remco@code010.nl>
2024-10-08 09:58:55 +0200

    #listitems from 10 > 25, styling fixes redesign

--- app/apps/main/views.py
+++ app/apps/main/views.py
@@ -144,7 +144,7 @@ def melding_lijst(request):
     gebruiker_context = get_gebruiker_context(gebruiker)
 
     standaard_waardes = {
-        "limit": "10",
+        "limit": "25",
         "ordering": get_ordering(gebruiker),
         "foldout_states": "[]",
     }
