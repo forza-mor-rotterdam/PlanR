1a4b42e7958ae9e97056181b7f1c2706431858d7
Remco Hoff <remco@code010.nl>
2024-10-15 14:39:35 +0200

    Styling charactercount

--- app/frontend/assets/controllers/characterCount_controller.js
+++ app/frontend/assets/controllers/characterCount_controller.js
@@ -1,21 +1,34 @@
 import { Controller } from '@hotwired/stimulus'
 
 let maxCharacterElement = null
+let maxCharactersNum = 0
 const maxCharacterPrefix = 'Aantal karakters: '
 
 export default class extends Controller {
   connect() {
     if (this.element.maxLength >= 0) {
+      maxCharactersNum = this.element.maxLength
       maxCharacterElement = document.createElement('small')
       maxCharacterElement.classList.add('help-block', 'no-margin')
-      maxCharacterElement.innerHTML = `${maxCharacterPrefix}${this.element.value.length}/${this.element.maxLength}`
+      maxCharacterElement.innerHTML = `${maxCharacterPrefix}${this.element.value.length}/${maxCharactersNum}`
       this.element.parentNode.insertBefore(maxCharacterElement, this.element.nextSibling)
     }
   }
 
   updateCharacterCount(count) {
     if (maxCharacterElement) {
-      maxCharacterElement.innerHTML = `${maxCharacterPrefix}${count}/${this.element.maxLength}`
+      if (maxCharactersNum - count >= 0) {
+        maxCharacterElement.classList.remove('error')
+        if (maxCharactersNum - count <= 10) {
+          maxCharacterElement.classList.add('warning')
+        } else {
+          maxCharacterElement.classList.remove('warning', 'error')
+        }
+      } else {
+        maxCharacterElement.classList.remove('warning')
+        maxCharacterElement.classList.add('error')
+      }
+      maxCharacterElement.innerHTML = `${maxCharacterPrefix}${count}/${maxCharactersNum}`
     }
   }
 
