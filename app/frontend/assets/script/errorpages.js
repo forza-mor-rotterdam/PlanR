f17108876d8f4aadb5324e1307a17bbc96f3d13e
Remco Hoff <remco@code010.nl>
2024-10-21 14:15:23 +0200

    Fix e-mailaddress, cleanup code

--- app/frontend/assets/script/errorpages.js
+++ app/frontend/assets/script/errorpages.js
@@ -1,7 +1,6 @@
 document.body.classList.remove('no-js')
 
 document.querySelector('#showAction').addEventListener('click', () => {
-  console.log('klik')
   const containers = document.querySelectorAll('.container__error')
 
   containers.forEach((container) => {
@@ -10,7 +9,6 @@ document.querySelector('#showAction').addEventListener('click', () => {
 })
 
 document.querySelector('#sendEmail').addEventListener('click', (e) => {
-  console.log(e.target)
   e.target.classList.add('disabled')
   document.querySelector('.btn-action-v2.hidden').classList.remove('hidden')
 })
