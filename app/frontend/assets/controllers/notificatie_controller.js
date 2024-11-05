67bcbfeb1e49cce640ca5f1c3e4bbf4513ae4df7
Remco Hoff <remco@code010.nl>
2024-11-05 15:15:55 +0100

    Animating snack in and out

--- app/frontend/assets/controllers/notificatie_controller.js
+++ app/frontend/assets/controllers/notificatie_controller.js
@@ -1,9 +1,37 @@
 import { Controller } from '@hotwired/stimulus'
 
 export default class extends Controller {
-  connect() {}
+  connect() {
+    const list = this.element.querySelectorAll('li')
+    // eslint-disable-next-line for-direction
+    for (let i = list.length - 1; i >= 0; i--) {
+      setTimeout(
+        () => {
+          list[i].classList.add('init')
+        },
+        2000 * (-i + list.length)
+      )
+      setTimeout(
+        () => {
+          list[i].classList.replace('init', 'show')
+        },
+        2000 * (-i + list.length + 1)
+      )
+    }
+  }
 
   hideNotification(e) {
-    e.target.closest('.notification').classList.add('hide')
+    const notification = e.target.closest('.notification')
+    notification.classList.add('hide')
+
+    notification.addEventListener('transitionend', () => {
+      notification.remove()
+    })
+  }
+
+  showAll(e) {
+    if (e.target === this.element) {
+      // this.element.classList.add('showAll')
+    }
   }
 }
