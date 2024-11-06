b651a7aa6c247fb4fd56304f5ee5edb99e1231a3
Maurice Guikema <maurice@mgui.nl>
2024-11-06 16:22:11 +0100

    main controller reloads notifications

--- app/frontend/assets/controllers/main_controller.js
+++ app/frontend/assets/controllers/main_controller.js
@@ -4,6 +4,44 @@ export default class extends Controller {
     if (this.getBrowser().includes('safari')) {
       document.body.classList.add('css--safari')
     }
+
+    const observerOptions = {
+      childList: true,
+      subtree: true,
+    }
+
+    this.notificationsTurboFrame = document.getElementById('notificatie_lijst_public')
+    this.notificationsTurboFrameReloadTimeout = null
+
+    setTimeout(() => {
+      const observer = new MutationObserver((records) => {
+        const turboFrames = records
+          .filter(
+            (record) => record?.target?.nodeName == 'TURBO-FRAME' && record?.addedNodes.length > 0
+          )
+          .map((record) => record)
+        if (turboFrames.length > 0) {
+          this.reloadNotificationsTurboFrame()
+        }
+      })
+      observer.observe(this.element, observerOptions)
+    }, 1000)
+
+    document.addEventListener('turbo:frame-missing', (event) => {
+      const {
+        detail: { response, visit },
+      } = event
+      event.preventDefault()
+      console.error('Content missing', response.url, visit)
+    })
+  }
+  reloadNotificationsTurboFrame() {
+    if (!this.notificationsTurboFrameReloadTimeout && this.notificationsTurboFrame) {
+      this.notificationsTurboFrameReloadTimeout = setTimeout(() => {
+        this.notificationsTurboFrame.reload()
+        this.notificationsTurboFrameReloadTimeout = null
+      }, 200)
+    }
   }
 
   getBrowser() {
