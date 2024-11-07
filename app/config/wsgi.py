91f829891000ee87e5e20e1afb058cce868aa6e6
Maurice Guikema <maurice@mgui.nl>
2024-11-07 16:22:52 +0100

    OpenTelemetryMiddleware for Dynatrace

--- app/config/wsgi.py
+++ app/config/wsgi.py
@@ -1,7 +1,9 @@
 import os
 
 from django.core.wsgi import get_wsgi_application
+from opentelemetry.instrumentation.wsgi import OpenTelemetryMiddleware
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
 application = get_wsgi_application()
+application = OpenTelemetryMiddleware(application)
