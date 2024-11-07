bcdece56320ff9d9300930374d3bf16eaca744d8
Maurice Guikema <maurice@mgui.nl>
2024-11-07 11:50:17 +0100

    dashboard & beheer urls to apps

---
+++ app/apps/beheer/urls.py
@@ -0,0 +1,158 @@
+from apps.authenticatie.views import (
+    GebruikerAanmakenView,
+    GebruikerAanpassenView,
+    GebruikerLijstView,
+    gebruiker_bulk_import,
+)
+from apps.authorisatie.views import (
+    RechtengroepAanmakenView,
+    RechtengroepAanpassenView,
+    RechtengroepLijstView,
+    RechtengroepVerwijderenView,
+)
+from apps.beheer.views import beheer
+from apps.context.views import (
+    ContextAanmakenView,
+    ContextAanpassenView,
+    ContextLijstView,
+    ContextVerwijderenView,
+)
+from apps.main.views import (
+    StandaardExterneOmschrijvingAanmakenView,
+    StandaardExterneOmschrijvingAanpassenView,
+    StandaardExterneOmschrijvingLijstView,
+    StandaardExterneOmschrijvingVerwijderenView,
+    TaaktypeCategorieAanmakenView,
+    TaaktypeCategorieAanpassenView,
+    TaaktypeCategorieLijstView,
+    TaaktypeCategorieVerwijderenView,
+)
+from apps.release_notes.views import (
+    ReleaseNoteAanmakenView,
+    ReleaseNoteAanpassenView,
+    ReleaseNoteListView,
+    ReleaseNoteVerwijderenView,
+)
+from django.urls import path
+
+urlpatterns = [
+    path("", beheer, name="beheer"),
+    path(
+        "gebruiker/",
+        GebruikerLijstView.as_view(),
+        name="gebruiker_lijst",
+    ),
+    path(
+        "gebruiker/bulk-import/",
+        gebruiker_bulk_import,
+        name="gebruiker_bulk_import",
+    ),
+    path(
+        "gebruiker/aanmaken/",
+        GebruikerAanmakenView.as_view(),
+        name="gebruiker_aanmaken",
+    ),
+    path(
+        "gebruiker/<int:pk>/aanpassen/",
+        GebruikerAanpassenView.as_view(),
+        name="gebruiker_aanpassen",
+    ),
+    path("context/", ContextLijstView.as_view(), name="context_lijst"),
+    path(
+        "context/aanmaken/",
+        ContextAanmakenView.as_view(),
+        name="context_aanmaken",
+    ),
+    path(
+        "context/<int:pk>/aanpassen/",
+        ContextAanpassenView.as_view(),
+        name="context_aanpassen",
+    ),
+    path(
+        "context/<int:pk>/verwijderen/",
+        ContextVerwijderenView.as_view(),
+        name="context_verwijderen",
+    ),
+    path(
+        "rechtengroep/",
+        RechtengroepLijstView.as_view(),
+        name="rechtengroep_lijst",
+    ),
+    path(
+        "rechtengroep/aanmaken/",
+        RechtengroepAanmakenView.as_view(),
+        name="rechtengroep_aanmaken",
+    ),
+    path(
+        "rechtengroep/<int:pk>/aanpassen/",
+        RechtengroepAanpassenView.as_view(),
+        name="rechtengroep_aanpassen",
+    ),
+    path(
+        "rechtengroep/<int:pk>/verwijderen/",
+        RechtengroepVerwijderenView.as_view(),
+        name="rechtengroep_verwijderen",
+    ),
+    # Standaard Tekst
+    path(
+        "standaardtekst/",
+        StandaardExterneOmschrijvingLijstView.as_view(),
+        name="standaard_externe_omschrijving_lijst",
+    ),
+    path(
+        "standaardtekst/aanmaken/",
+        StandaardExterneOmschrijvingAanmakenView.as_view(),
+        name="standaard_externe_omschrijving_aanmaken",
+    ),
+    path(
+        "standaardtekst/<int:pk>/aanpassen/",
+        StandaardExterneOmschrijvingAanpassenView.as_view(),
+        name="standaard_externe_omschrijving_aanpassen",
+    ),
+    path(
+        "standaardtekst/<int:pk>/verwijderen/",
+        StandaardExterneOmschrijvingVerwijderenView.as_view(),
+        name="standaard_externe_omschrijving_verwijderen",
+    ),
+    # Taaktype categorie
+    path(
+        "taaktype-categorie/",
+        TaaktypeCategorieLijstView.as_view(),
+        name="taaktype_categorie_lijst",
+    ),
+    path(
+        "taaktype-categorie/aanmaken/",
+        TaaktypeCategorieAanmakenView.as_view(),
+        name="taaktype_categorie_aanmaken",
+    ),
+    path(
+        "taaktype-categorie/<int:pk>/aanpassen/",
+        TaaktypeCategorieAanpassenView.as_view(),
+        name="taaktype_categorie_aanpassen",
+    ),
+    path(
+        "taaktype-categorie/<int:pk>/verwijderen/",
+        TaaktypeCategorieVerwijderenView.as_view(),
+        name="taaktype_categorie_verwijderen",
+    ),
+    path(
+        "release-notes/",
+        ReleaseNoteListView.as_view(),
+        name="release_note_lijst",
+    ),
+    path(
+        "release-notes/aanmaken/",
+        ReleaseNoteAanmakenView.as_view(),
+        name="release_note_aanmaken",
+    ),
+    path(
+        "release-notes/<int:pk>/aanpassen/",
+        ReleaseNoteAanpassenView.as_view(),
+        name="release_note_aanpassen",
+    ),
+    path(
+        "release-notes/<int:pk>/verwijderen/",
+        ReleaseNoteVerwijderenView.as_view(),
+        name="release_note_verwijderen",
+    ),
+]
