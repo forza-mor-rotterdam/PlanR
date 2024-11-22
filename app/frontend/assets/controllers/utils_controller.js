c32e958b5e1c2174facffc92cff63ccbb6c0b54f
Remco Hoff <remco@code010.nl>
2024-11-22 12:06:49 +0100

    Show and hide notificationcenter

--- app/frontend/assets/controllers/utils_controller.js
+++ app/frontend/assets/controllers/utils_controller.js
@@ -8,4 +8,14 @@ export default class extends Controller {
   goToUrl(e) {
     window.location.href = e.params.url
   }
+
+  noHover(e) {
+    const targetContainer = e.params.targetcontainer ?? 'container__uitklapper'
+    this.element.closest(`.${targetContainer}`).classList.add('no-hover')
+
+    setTimeout(() => {
+      this.element.blur()
+      this.element.closest(`.${targetContainer}`).classList.remove('no-hover')
+    }, 500)
+  }
 }
