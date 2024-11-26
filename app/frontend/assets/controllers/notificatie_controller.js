d751ab262270f6d1b9986c2639d92789ca19b720
Remco Hoff <remco@code010.nl>
2024-11-26 16:09:08 +0100

    Refactoring notification bars

--- app/frontend/assets/controllers/notificatie_controller.js
+++ app/frontend/assets/controllers/notificatie_controller.js
@@ -1,8 +1,5 @@
 import { Controller } from '@hotwired/stimulus'
 
-let initialTouchX,
-  finalTouchX,
-  deltaX = null
 const SWIPE_TRESHOLD = 100
 const MAX_CHARACTERS = 200
 export default class extends Controller {
@@ -13,6 +10,10 @@ export default class extends Controller {
   }
 
   initialize() {
+    let self = this
+    self.initialTouchX = null
+    self.finalTouchX = null
+    self.deltaX = null
     let contentString = null
     let truncatedString = null
     if (this.hasContentTarget) {
@@ -44,21 +45,21 @@ export default class extends Controller {
     if ('ontouchstart' in window) {
       this.element.addEventListener('touchstart', (event) => {
         event.preventDefault()
-        initialTouchX = event.touches[0].clientX
+        self.initialTouchX = event.touches[0].clientX
         const currentWidth = this.element.clientWidth
         this.element.style.width = `${currentWidth}px`
       })
 
       this.element.addEventListener('touchmove', (event) => {
         event.preventDefault()
-        deltaX = initialTouchX - event.changedTouches[0].clientX
-        this.element.style.marginLeft = `-${deltaX}px`
+        self.deltaX = self.initialTouchX - event.changedTouches[0].clientX
+        this.element.style.marginLeft = `-${self.deltaX}px`
       })
 
       this.element.addEventListener('touchend', (event) => {
         event.preventDefault()
-        finalTouchX = event.changedTouches[0].clientX
-        if (deltaX < SWIPE_TRESHOLD) {
+        self.finalTouchX = event.changedTouches[0].clientX
+        if (self.deltaX < SWIPE_TRESHOLD) {
           this.element.style.marginLeft = 0
         }
         if (event.target.classList.contains('btn-close--small')) {
@@ -68,13 +69,14 @@ export default class extends Controller {
             this[`${event.target.getAttribute('data-action').split('#')[1]}`]()
           }
         } else {
-          this.handleSwipe(initialTouchX, finalTouchX)
+          this.handleSwipe(self.initialTouchX, self.finalTouchX)
         }
       })
 
       window.addEventListener('click', () => {
         if (this.element.closest('.container__notification')) {
-          this.element.closest('.container__notification').classList.remove('showAll')
+          this.element.closest('.container__notification').classList.remove('expanded')
+          this.element.closest('.container__notification').classList.add('collapsed')
         }
       })
     }
@@ -83,7 +85,8 @@ export default class extends Controller {
   disconnect() {
     if ('ontouchstart' in window) {
       window.removeEventListener('click', () => {
-        this.element.closest('.container__notification').classList.remove('showAll')
+        this.element.closest('.container__notification').classList.remove('expanded')
+        this.element.closest('.container__notification').classList.add('collapsed')
       })
     }
   }
@@ -97,31 +100,21 @@ export default class extends Controller {
     })
   }
 
-  dispatchRedraw() {
-    this.element.dispatchEvent(
-      new CustomEvent('notificatieVerwijderd', {
-        bubbles: true,
-      })
-    )
-  }
   hideNotification() {
     const notificatie = this.element
     notificatie.classList.add('hide')
-
-    notificatie.addEventListener('transitionend', () => {
-      if (notificatie.nodeName === 'TURBO-FRAME') {
-        notificatie.setAttribute('src', notificatie.getAttribute('data-src'))
-      }
-      this.dispatchRedraw()
+    if (notificatie.nodeName === 'TURBO-FRAME') {
+      notificatie.setAttribute('src', notificatie.getAttribute('data-src'))
       notificatie.remove()
-    })
+    }
   }
 
   handleSwipe() {
-    if (initialTouchX - finalTouchX > SWIPE_TRESHOLD) {
+    let self = this
+    this.element.closest('.container__notification').classList.remove('collapsed')
+    this.element.closest('.container__notification').classList.add('expanded')
+    if (self.initialTouchX - self.finalTouchX > SWIPE_TRESHOLD) {
       this.hideNotification()
-    } else {
-      this.element.closest('.container__notification').classList.add('showAll')
     }
   }
 }
--- app/frontend/assets/controllers/notificatielijst_controller.js
+++ app/frontend/assets/controllers/notificatielijst_controller.js
@@ -4,76 +4,20 @@ export default class extends Controller {
   static targets = ['notificatie']
 
   connect() {
-    this.setList(this.element.classList.value.includes('toast'))
-    this.element.addEventListener('notificatieVerwijderd', () => {
-      // wacht tot notificatie echt is verwijderd
+    if (!this.element.classList.value.includes('toast')) {
+      this.element.addEventListener('mouseover', () => {
+        this.element.classList.remove('collapsed')
+        this.element.classList.add('expanded')
+      })
+      this.element.addEventListener('mouseleave', () => {
+        this.element.classList.remove('expanded')
+        this.element.classList.add('collapsed')
+      })
       setTimeout(() => {
-        this.resetList(this.element.classList.value.includes('toast'))
-      }, 100)
-    })
-  }
-
-  setList(isToast) {
-    const list = this.notificatieTargets
-    // eslint-disable-next-line for-direction
-    for (let i = list.length - 1; i >= 0; i--) {
-      console.log(i, ' - 200 * (-i + list.length)', 200 * (-i + list.length))
-      setTimeout(
-        () => {
-          list[i].classList.add('init')
-          if (i === 0) {
-            this.element.classList.remove('busy')
-          }
-        },
-        200 * (-i + list.length)
-      )
-    }
-    // for (let i = 0; i < list.length; i++) {
-    //   setTimeout(() => {
-    //     list[i].classList.add('init')
-    //     if (i === 0) {
-    //       this.element.classList.remove('busy')
-    //     }
-    //   }, 600 * i)
-    // }
-
-    if (!isToast) {
-      this.element.classList.add('busy')
-      // Alleen als het geen toast is achter elkaar tonen
-      const timeToLeave = list.length > 5 ? list.length * 400 : 5000
-      console.log('timeToLeave', timeToLeave)
-      for (let i = 0; i < list.length; i++) {
-        setTimeout(
-          () => {
-            list[i].classList.replace('init', 'show')
-            if (i === 0) {
-              list[i].style.transform = `translateY(0) scale(1, 1)`
-            } else {
-              list[i].style.transform = `translateY(-${
-                list[i].offsetTop - list[0].offsetHeight
-              }px) translateY(-100%) translateY(${i * 8}px) scale(${1 - i * 0.02}, 1)`
-            }
-          },
-          timeToLeave + 100 * i
-        )
-      }
-    }
-  }
-
-  resetList(isToast) {
-    if (!isToast) {
-      // Alleen als het geen toast is achter elkaar tonen
-      const list = this.notificatieTargets
-      console.log('list', list)
-      for (let i = 0; i < list.length; i++) {
-        if (i === 0) {
-          list[i].style.transform = `translateY(0) scale(1, 1)`
-        } else {
-          list[i].style.transform = `translateY(-${
-            list[i].offsetTop - list[0].offsetHeight
-          }px) translateY(-100%) translateY(${i * 8}px) scale(${1 - i * 0.02}, 1)`
+        if (!this.element.classList.contains('expanded')) {
+          this.element.classList.add('collapsed')
         }
-      }
+      }, 5000)
     }
   }
 }
