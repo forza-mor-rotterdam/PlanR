e1f60438cc7a665571bc2b41a532275030aab3f1
Remco Hoff <remco@code010.nl>
2024-11-28 10:32:56 +0100

    Refactoring readmore

--- app/frontend/assets/controllers/notificaties/snack_overzicht_item_controller.js
+++ app/frontend/assets/controllers/notificaties/snack_overzicht_item_controller.js
@@ -1,15 +1,36 @@
 import { Controller } from '@hotwired/stimulus'
 
+const MAX_CHARACTERS = 100
 export default class extends Controller {
+  static targets = ['content']
+
   connect() {
     this.element.controller = this
+    this.contentString = null
     this.manager = null
+    let truncatedString = null
     console.log(`${this.identifier} connected`)
+    if (this.hasContentTarget) {
+      this.contentString = this.contentTarget.innerText
+      if (this.contentString.length > MAX_CHARACTERS) {
+        truncatedString = `${this.contentString.slice(
+          0,
+          MAX_CHARACTERS - 13
+        )}... <a href="" data-action="notificaties--snack-overzicht-item#readMore">Lees meer</a>`
+        this.contentTarget.innerHTML = truncatedString
+        this.contentTarget.style.height = `${this.contentTarget.clientHeight}px`
+      }
+    }
   }
   initializeManager(manager) {
     this.manager = manager
   }
   markeerAlsGelezen() {
     this.element.classList.add('is-watched')
+  }
+  readMore(e = null) {
+    e?.preventDefault()
+    this.contentTarget.innerText = this.contentString
+    this.contentTarget.style.height = `${this.contentTarget.scrollHeight}px`
   }
 }
