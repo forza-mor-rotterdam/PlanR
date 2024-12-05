0e4f7346a379bc32326eb23295a8df6af504ea2a
Remco Hoff <remco@code010.nl>
2024-12-05 15:39:07 +0100

    Cleanup code

--- app/frontend/assets/controllers/utils_controller.js
+++ app/frontend/assets/controllers/utils_controller.js
@@ -4,15 +4,4 @@ export default class extends Controller {
   goToUrl(e) {
     window.location.href = e.params.url
   }
-
-  noHover(e) {
-    // used to close popup with close-button while hovering
-    const targetContainer = e?.params?.targetcontainer ?? 'container__uitklapper'
-    this.element.closest(`.${targetContainer}`)?.classList.remove('show')
-    this.element.closest(`.${targetContainer}`)?.classList.add('no-hover')
-    this.element.blur()
-    setTimeout(() => {
-      this.element.closest(`.${targetContainer}`)?.classList.remove('no-hover')
-    }, 500)
-  }
 }
