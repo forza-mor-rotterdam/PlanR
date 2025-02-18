bdf7254bff13a3fabce9b815806d4dad2d99cb08
Maurice Guikema <maurice@mgui.nl>
2025-02-18 10:38:35 +0100

    image slider and modal image slider controllers ready

--- app/frontend/assets/controllers/image_slider_controller.js
+++ app/frontend/assets/controllers/image_slider_controller.js
@@ -1,46 +1,17 @@
 import { Controller } from '@hotwired/stimulus'
 
 export default class extends Controller {
-  static outlets = ['modal-image-slider', 'modal']
+  static outlets = ['modal-image-slider']
   static values = {
     data: String,
   }
-  static targets = [
-    'selectedImage',
-    'selectedImageModal',
-    'thumbList',
-    'imageSliderThumbContainer',
-    'modalImages',
-    'imageSliderWidth',
-    'navigateImagesLeft',
-    'navigateImagesRight',
-    'imageCounter',
-
-    'thumb',
-    'image',
-    'imageSliderContainer',
-  ]
-
+  static targets = ['thumb', 'thumbContainer', 'image', 'imageSliderContainer']
   initialize() {
     this.selectedCls = 'selected'
     this.bijlagen = JSON.parse(this.dataValue)
   }
-  modalImageSliderOutletConnected(outlet) {
-    console.log('modalImageSliderOutletConnected from: ', this.identifier)
-    console.log(outlet)
+  modalImageSliderOutletConnected() {
     this.modalImageSliderOutlet.init(this)
-  }
-  modalOutletConnected(outlet) {
-    console.log('modal outlet')
-    console.log(outlet)
-  }
-  connect() {
-    console.log('MODAL OUTLET')
-    console.log(this.hasModalImageSliderOutlet)
-    console.log(this.hasModalOutlet)
-    // console.log(this.modalImageSliderOutlet)
-    // console.log(this.modalImageSliderOutlets)
-    // console.log(JSON.parse(this.dataValue))
   }
   thumbTargetConnected() {
     this.thumbTarget.classList.add(this.selectedCls)
@@ -48,6 +19,13 @@ export default class extends Controller {
   selectThumbHandler(e) {
     const selectedThumb = e.target.closest('li')
     this.updateSlider(selectedThumb.dataset.imageUuid)
+  }
+  onScrollSliderHandler() {
+    this.updateSlider(
+      Math.floor(
+        this.imageSliderContainerTarget.scrollLeft / this.imageSliderContainerTarget.offsetWidth
+      )
+    )
   }
   updateSlider(imageUuid) {
     this.thumbTargets.map((elem) =>
@@ -61,15 +39,6 @@ export default class extends Controller {
       left: Number(index) * this.imageSliderContainerTarget.offsetWidth,
       top: 0,
     })
-  }
-  showImage() {
-    // this.selectedImageModalTarget.style.backgroundImage = `url('${this.urlPrefixValue}${imagesList[selectedImageIndex]}')`
-    // this.showHideImageNavigation()
-    // this.imageCounterTarget.textContent = `Foto ${selectedImageIndex + 1} van ${imagesList.length}`
-    // this.imageScrollInView(selectedImageIndex) //image in detailpage
-    // fullSizeImageContainer = this.selectedImageModalTarget
-    // this.showNormal()
-    // window.addEventListener('mousemove', this.getRelativeCoordinates, true)
-    // this.selectedImageModalTarget.addEventListener('click', this.showLarge)
+    selectedThumb.scrollIntoView({ behavior: 'smooth', block: 'nearest', inline: 'center' })
   }
 }
--- app/frontend/assets/controllers/modal_image_slider_controller.js
+++ app/frontend/assets/controllers/modal_image_slider_controller.js
@@ -2,37 +2,74 @@ import { Controller } from '@hotwired/stimulus'
 
 export default class extends Controller {
   static values = {
-    data: String,
     initialImageUuid: String,
   }
-  static targets = [
-    'selectedImage',
-    'selectedImageModal',
-    'thumbList',
-    'imageSliderThumbContainer',
-    'modalImages',
-    'imageSliderWidth',
-    'navigateImagesLeft',
-    'navigateImagesRight',
-    'imageCounter',
-  ]
+  static targets = ['image', 'nav', 'next', 'previous', 'counter', 'label']
 
   initialize() {
     this.selectedCls = 'selected'
+    this.imageUuid = this.initialImageUuidValue
   }
-  connect() {
-    console.log(this.identifier)
+  disconnect() {
+    const imageUuid = this.imageUuids[this.imageIndex]
+    this.imageSliderController.updateSlider(imageUuid)
   }
-
   init(imageSliderController) {
     this.imageSliderController = imageSliderController
-    console.log('this.imageSliderController.bijlagen.length')
-    console.log(this.imageSliderController.bijlagen.length)
+    this.imageUuids = this.imageSliderController.bijlagen.map((bijlage) => bijlage.uuid)
+    this.imageIndex = this.imageUuids.indexOf(this.imageUuid)
+    this.setNavState()
+    this.imageTarget.addEventListener('mousemove', (e) => this.getRelativeCoordinates(e), true)
+  }
+  imageClickToggleHandler() {
+    this.element.classList.toggle('fullSize')
+    if (this.element.classList.contains('fullSize')) {
+      this.imageTarget.style.backgroundPosition = '50% 50%'
+    }
+  }
+  nextHandler() {
+    this.updateImage(1)
+  }
+  previousHandler() {
+    this.updateImage(-1)
+  }
+  bijlage() {
+    return this.imageSliderController.bijlagen.find(
+      (bijlage) => bijlage.uuid === this.imageUuids[this.imageIndex]
+    )
+  }
+  updateImage(extraIndex) {
+    if (
+      this.imageIndex + extraIndex >= this.imageUuids.length ||
+      this.imageIndex + extraIndex < 0
+    ) {
+      return
+    }
+    this.imageIndex = this.imageIndex + extraIndex
+
+    this.setNavState()
+
+    this.imageTarget.style.backgroundImage = `url(/core${this.bijlage().afbeelding_relative_url})`
+  }
+  setNavState() {
+    this.nextTarget.classList[this.imageIndex >= this.imageUuids.length - 1 ? 'add' : 'remove'](
+      'inactive'
+    )
+    this.previousTarget.classList[this.imageIndex <= 0 ? 'add' : 'remove']('inactive')
+
+    this.counterTarget.textContent = `Foto ${this.imageIndex + 1} van ${this.imageUuids.length}`
+    this.labelTarget.textContent = this.bijlage().label
   }
 
-  getRelativeCoordinates() {}
-
-  showLarge() {}
-
-  showNormal() {}
+  getRelativeCoordinates(e) {
+    if (this.element.classList.contains('fullSize')) {
+      const rect = this.imageTarget.getBoundingClientRect()
+      this.imageTarget.style.backgroundPosition = `
+        ${((e.clientX - rect.left) * 100) / this.imageTarget.offsetWidth}%
+        ${((e.clientY - rect.top) * 100) / this.imageTarget.offsetHeight}%
+        `
+    } else {
+      this.imageTarget.style.backgroundPosition = '50% 50%'
+    }
+  }
 }
