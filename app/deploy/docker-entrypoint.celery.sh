1c0b8781146c355a38be21398ace0cb923c88398
Maurice Guikema <maurice@mgui.nl>
2025-06-19 13:26:05 +0200

    sigterm trap for celery worker

--- app/deploy/docker-entrypoint.celery.sh
+++ app/deploy/docker-entrypoint.celery.sh
@@ -2,6 +2,16 @@
 set -euo pipefail   # crash on missing env variables, stop on any error, and exit if a pipe fails
 set -x   # Enable verbose output for debugging
 
+_term() {
+  echo "Caught SIGTERM signal!"
+  kill -TERM "$child" 2>/dev/null
+}
+
+trap _term SIGTERM
+
 # Initialize celery worker
 celery -A config beat -l INFO --scheduler django_celery_beat.schedulers:DatabaseScheduler --detach
-celery -A config worker -l info
+celery -A config worker -l info &
+
+child=$!
+wait "$child"
