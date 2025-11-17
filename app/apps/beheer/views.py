2cc83d27383133806d8cb91f6a2c0b9c5b22ee43
Maurice Guikema <maurice@mgui.nl>
2025-11-17 19:55:00 +0100

    taken aanmaken try fix formset

--- app/apps/beheer/views.py
+++ app/apps/beheer/views.py
@@ -21,7 +21,6 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required, permission_required
 from django.contrib.auth.mixins import PermissionRequiredMixin
 from django.contrib.messages.views import SuccessMessageMixin
-from django.http import HttpResponse
 from django.shortcuts import redirect, render
 from django.urls import reverse, reverse_lazy
 from django.views.generic import (
@@ -458,12 +457,7 @@ class TaakopdrachtTaakAanmakenIssueLijstView(PermissionRequiredMixin, FormView):
     page_session_key = "taakopdrachten_taak_aanmaken_issues__page"
     q_session_key = "taakopdrachten_taak_aanmaken_issues__q"
 
-    def get(self, request, *args, **kwargs):
-        return HttpResponse("taakopdrachten_taak_aanmaken_issues")
-        # return super().get(request, *args, **kwargs)
-
     def dispatch(self, request, *args, **kwargs):
-        print("DISPATCH")
         taakapplicaties_response = MORCoreService().applicaties(
             params={"applicatie_type": "taakapplicatie"}, cache_timeout=60 * 60
         )
